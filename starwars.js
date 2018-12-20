d90cf0185349cd3fe9aa71d3416471ca5dfbb316
Derek Houck <derek@derekhouck.com>
2018-12-20 11:30:01 -0800

    Add total vehicles and starships. Write error message.

--- starwars.js
+++ starwars.js
@@ -4,12 +4,21 @@ const axios = require('axios');
 const searchTerm = process.argv[2];
 const BASE_URL = 'https://swapi.co/api';
 
+function possessivePronoun(character) {
+  return character.gender === 'male' ? 'He has' : character.gender === 'female' ? 'She has' : 'They have';
+}
+
 axios.get(`${BASE_URL}/people/?search=${searchTerm}`)
   .then(results => {
     const character = results.data.results[0];
 
+    if (!character) {
+      throw new Error(`${searchTerm} is not an entity we're looking for. Move along.`);
+      
+    }
+
     console.log(`${character.name} has been found!`);
-    console.log(`${character.gender === 'male' ? 'He has' : character.gender === 'female' ? 'She has' : 'They have'} starred in the following films:`);
+    console.log(`${possessivePronoun(character)} starred in the following films:`);
 
     const films = character.films;
     const filmPromises = films.map(film => {
@@ -24,6 +33,7 @@ axios.get(`${BASE_URL}/people/?search=${searchTerm}`)
         for (let i = 0; i < data.length; i++) {
           console.log(`${i + 1}. ${data[i].title}`);
         }
+        console.log(`${possessivePronoun(character)} has also been associated with a total of ${character.vehicles.length} vehicles and ${character.starships.length} starships.`);
       });
   })
   .catch(err => console.log(err.message));
