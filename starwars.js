715b83b8f597b8b7aad00d48a86cb0420a8593f2
Derek Houck <derek@derekhouck.com>
2018-12-20 08:47:17 -0800

    Use search parameter on /people endpoint to find searched character

--- starwars.js
+++ starwars.js
@@ -2,5 +2,11 @@
 
 const axios = require('axios');
 const searchTerm = process.argv[2];
+const BASE_URL = 'https://swapi.co/api';
 
-console.log(searchTerm);
+axios.get(`${BASE_URL}/people/?search=${searchTerm}`)
+  .then(results => {
+    const character = results.data.results[0];
+    console.log(character);
+  })
+  .catch(err => console.log(err.message));
