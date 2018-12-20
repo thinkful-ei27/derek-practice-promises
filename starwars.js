ba32ea2af7c38452d0bb86c1106ac40aaefb783b
Derek Houck <derek@derekhouck.com>
2018-12-20 09:34:06 -0800

    Make parrellel requests for each film and compile the data

--- starwars.js
+++ starwars.js
@@ -7,6 +7,23 @@ const BASE_URL = 'https://swapi.co/api';
 axios.get(`${BASE_URL}/people/?search=${searchTerm}`)
   .then(results => {
     const character = results.data.results[0];
-    console.log(character);
+
+    console.log(`${character.name} has been found!`);
+    console.log(`${character.gender === 'male' ? 'He has' : character.gender === 'female' ? 'She has' : 'They have'} starred in the following films:`);
+
+    const films = character.films;
+    const filmPromises = films.map(film => {
+      return axios.get(film);
+    });
+    Promise.all(filmPromises)
+      .then(results => {
+        const data = results.map(result => result.data);
+        data.sort((a, b) => {
+          return new Date(a.release_date) - new Date(b.release_date);
+        });
+        for (let i = 0; i < data.length; i++) {
+          console.log(`${i + 1}. ${data[i].title}`);
+        }
+      });
   })
   .catch(err => console.log(err.message));
